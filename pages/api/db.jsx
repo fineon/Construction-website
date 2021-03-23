179a8fbedb72ed93716a2db5b31781b1bbd8efe9
Ian <ian@heythereian.com>
2021-03-23 15:47:48 -0700

    added serverless-mysql for db queries and CRUD

--- pages/api/db.jsx
+++ pages/api/db.jsx
@@ -1,3 +1,15 @@
-// export default function db(req, res) {
+import {queryFunc} from '../../lib/mysql-init'
 
-// }
+export default function db(req, res) {
+    try{
+        if(!req){
+            return res.status(400).json({warning: 'must input values'})
+        } else {
+            const result = await queryFunc(`SELECT name FROM owners`)
+            return res.json(result)
+        }
+    } 
+    catch(e){
+        console.log(e)
+    }
+}

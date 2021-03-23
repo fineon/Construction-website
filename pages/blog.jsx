9624c26d20f50c413dcc8831236db5ff162f0078
Ian <ian@heythereian.com>
2021-03-22 22:47:30 -0700

    added some annotations for clarification

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Head from 'next/head'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import {Container, Col, Row, Image, Card, Button} from 'react-bootstrap'
+import { Container, Col, Row, Image, Card, Button } from 'react-bootstrap'
 
 export async function getStaticProps(context) {
 
+    //festching post data from WordPress CMS, with secrets hidden in .env.local file
     const posts = await fetch(process.env.WORDPRESS_URL)
     const postData = await posts.json()
 
     // console.log(data)
     if (!postData) {
-        return {notFound: true}
+        return { notFound: true }
     } else {
         return {
             props: {
@@ -22,14 +23,14 @@ export async function getStaticProps(context) {
 }
 
 function BlogPosts(props) {
-    return(
+    return (
         <Card className='m-4 p-3'>
             <Card.Title>
-            <h2>{props.content.title.rendered}</h2>
-            <h5>{new Date(props.content.date).toLocaleDateString()}</h5>
+                <h2>{props.content.title.rendered}</h2>
+                <h5>{new Date(props.content.date).toLocaleDateString()}</h5>
             </Card.Title>
             <Card.Body>
-            <div dangerouslySetInnerHTML={{__html: props.content.content.rendered}}/>
+                <div dangerouslySetInnerHTML={{ __html: props.content.content.rendered }} />
             </Card.Body>
         </Card>
     )
@@ -42,13 +43,13 @@ export default function blog(props) {
             <Head>
                 <title>Construction Co. Blog</title>
             </Head>
-            <Header blog={true}/>
+            <Header blog={true} />
 
-           <h1 className='my-5 text-center'>Blog</h1>
+            <h1 className='my-5 text-center'>Blog</h1>
+            {/* rendering posts with Bootstrap cards, with data returning from getStaticProps */}
+            {props.postData ? props.postData.map(item => <BlogPosts key={item.id} content={item} />) : null}
 
-            {props.postData? props.postData.map(item => <BlogPosts key={item.id} content={item}/>) : null}
-
-            <Footer/>
+            <Footer />
         </>
     )
 }
